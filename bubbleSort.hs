0e79541e76629b5c4ab9e6d8f1c99667d7ca81ee
kehukef <kehukef@gmail.com>
2017-10-03 01:04:53 +0000

    bubble sort!!

---
+++ bubbleSort.hs
@@ -0,0 +1,15 @@
+----------------
+-- バブルソート
+----------------
+bubblesort :: (Ord a) => [a] -> [a]
+bubblesort [] = []
+bubblesort xs = y : bubblesort ys
+  where
+    -- 小さい要素を右端へ寄せる
+    swp [x] = [x]
+    swp (x:y:zs)
+      | x < y     = y : swp (x:zs)
+      | otherwise = x : swp (y:zs)
+
+    -- 右端へ寄せたらひっくり返して先頭とそれ以外を束縛
+    (y:ys) = reverse $ swp xs
